33ee95016fb2a1feef89cebcfd60e9954665cec6
Abel Rodríguez <oarodriguez@live.com.mx>
2013-09-29 16:36:45 -0500

    Add main module file and basic information cmdlet.

---
+++ Info.ps1
@@ -0,0 +1,40 @@
+﻿<#
+    Copyright (c) 2013, Abel Rodríguez, see LICENSE for details.
+#>
+Set-StrictMode -Version Latest
+
+<#
+.Synopsis
+    Finds the directory of Anaconda base installation and return
+    its path.
+.DESCRIPTION
+    This cmdlet finds the directory where Anaconda base system is
+    installed (python, third party packages, etc.), and return its 
+    absolute path.
+.EXAMPLE
+    Get-CondaRoot
+    C:\Anaconda
+.EXAMPLE
+    Get-CondaRoot
+    C:\Path\To\Your\Anaconda\Installation    
+#>
+function Get-CondaRoot
+{
+    [CmdletBinding()]
+    [OutputType([String])]
+    Param()
+
+    Process
+    {
+        $infoRegex = '(?s) *root environment : (.*?)\n'
+        $condaInformation = conda info | Out-String
+
+        if($condaInformation -match $infoRegex) { $Matches[1] }
+        else 
+        {
+            Write-Error 'Anaconda root environment not found.'
+        }
+
+    }
+
+}
---
+++ PowerShellConda.psm1
@@ -0,0 +1,18 @@
+﻿<#
+    Copyright (c) 2013, Abel Rodríguez, see LICENSE for details.
+#>
+Set-StrictMode -Version Latest
+
+Push-Location $PSScriptRoot
+
+$condaCommandExists = (Get-Command conda -CommandType Application)
+if (!$condaCommandExists) 
+{
+    Write-Error 'Couldn''t find conda in your system. Check your PATH and import this module again.'
+}
+else 
+{    
+    . .\Info.ps1
+}
+
+Pop-Location
